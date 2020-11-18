3ef48d6ba4cfda19ab149b627c323f8c6bc6c6a6
Ivan Saranchonkau <i.saranchonkau@ozitag.com>
2020-11-18 20:22:17 +0300

    v0.2.7 - Fix image lazy loading

--- src/components/Image.tsx
+++ src/components/Image.tsx
@@ -1,9 +1,23 @@
 import React, { useRef } from 'react';
 
-type Props = React.ImgHTMLAttributes<HTMLImageElement>;
-
-const placeholder =
+/**
+ * Placeholder fixes broken image symbol: https://github.com/aFarkas/lazysizes#broken-image-symbol
+ *
+ * Reference: https://github.com/verlok/vanilla-lazyload/issues/212#issuecomment-397807313
+ *
+ * If you don't put anything in the `src`,
+ * when `lazysizes` copies the `data-src` in the `src`,
+ * the image is shown while loading by the browser.
+ *
+ * If you put something in the `src`,
+ * when `lazysizes` copies the `data-src` in the `src`,
+ * the browser waits until the new image is loaded, then replaces the image.
+ * That what we do when images are loaded lazily.
+ */
+const PLACEHOLDER =
   'data:image/gif;base64,R0lGODlhAQABAAAAACH5BAEKAAEALAAAAAABAAEAAAICTAEAOw==';
+
+type Props = React.ImgHTMLAttributes<HTMLImageElement>;
 
 function Image({ className, src, srcSet, loading = 'eager', ...rest }: Props) {
   const imageRef = useRef<HTMLImageElement>(null);
@@ -17,8 +31,8 @@ function Image({ className, src, srcSet, loading = 'eager', ...rest }: Props) {
     <img
       className={imgClassName}
       ref={imageRef}
-      src={isLazy ? placeholder : src}
-      srcSet={isLazy ? placeholder : srcSet}
+      src={isLazy ? PLACEHOLDER : src}
+      srcSet={isLazy ? undefined : srcSet}
       data-src={src}
       data-srcset={srcSet}
       alt=""
