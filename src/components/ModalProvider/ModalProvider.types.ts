e5a9f08e58c632777a5846bb510091aefdf88fca
Ivan Saranchonkau <i.saranchonkau@ozitag.com>
2020-12-15 13:41:04 +0300

    v0.3.6 - Add `withAnimation` option to `openModal` function

--- src/components/ModalProvider/ModalProvider.types.ts
+++ src/components/ModalProvider/ModalProvider.types.ts
@@ -2,18 +2,18 @@ import React from 'react';
 
 import { OverlayProps } from '../Overlay';
 
-export type State = {
+export interface State {
   type: React.ComponentType<ModalProps>;
   props: object;
   options?: OpenModalFunctionOptions;
-};
+}
 
 export interface CommonModalOptions {
   components?: { Overlay?: React.ComponentType<OverlayProps> };
   withAnimation?: boolean;
 }
 
-export type OpenModalFunctionOptions = CommonModalOptions;
+export interface OpenModalFunctionOptions extends CommonModalOptions {}
 
 export interface OpenModalFunction<P extends ModalProps> {
   (type: React.ComponentType<P>, props: P['innerProps']): void;
@@ -24,7 +24,7 @@ export interface OpenModalFunction<P extends ModalProps> {
   ): void;
 }
 
-export type ModalProps<T = {}> = {
+export interface ModalProps<T = {}> {
   closeModal: () => void;
   innerProps: T;
-};
+}
