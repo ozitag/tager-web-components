9b6a98d5da9ce187a95014bd184964178fbe0a51
PavelSerebryany <pavel97107@gmail.com>
2021-10-26 19:38:02 +0300

    v0.4.6 - added the ability to set keys for closing the modal window, as well as the ability to disable closing the modal window when clicking outside the content

--- src/components/ModalProvider/ModalProvider.types.ts
+++ src/components/ModalProvider/ModalProvider.types.ts
@@ -14,6 +14,8 @@ export interface CommonModalOptions {
   };
   withAnimation?: boolean;
   scrollLockDisabled?: boolean;
+  closeModalKeys?: Array<string> | string;
+  disableCloseByOutside?: boolean;
 }
 
 export type OpenModalFunctionOptions = CommonModalOptions;
