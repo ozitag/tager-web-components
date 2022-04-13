5a8d7b8d5e74b775b543c4be684f8a8d002ab286
Vital Ozierski <v.ozierski@ozitag.com>
2022-04-13 16:20:23 +0300

    0.4.13 - fix Spinner animation error in React 18

--- src/components/Spinner.tsx
+++ src/components/Spinner.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import styled, { Keyframes, keyframes } from 'styled-components';
+import styled, { keyframes } from 'styled-components';
 
 interface SpinnerProps {
   show: boolean;
@@ -37,34 +37,29 @@ const Inner = styled.div<{ size: number }>`
   height: ${(props) => props.size}px;
 `;
 
-function getAnimation(props: { thickness: number }): Keyframes {
-  return keyframes`
-  from {
-      top: 50%;
-      left: 50%;
-      width: ${props.thickness * 2}px;
-      height: ${props.thickness * 2}px;
-      opacity: 1;
-  }
-  
-  to {
-      top: 0;
-      left: 0;
-      width: 100%;
-      height: 100%;
-      opacity: 0;
-  }
-`;
-}
-
 const Circle = styled.div<{ thickness: number; color?: string }>`
   position: absolute;
 
-  border: ${(props) => props.thickness}px solid
-    ${(props) => props.color ?? 'white'};
+  border: ${(props) => props.thickness}px solid ${(props) => props.color ?? 'white'};
   opacity: 1;
   border-radius: 50%;
-  animation-name: ${(props) => getAnimation({ thickness: props.thickness })};
+  animation-name: ${props => keyframes`
+    from {
+        top: 50%;
+        left: 50%;
+        width: ${props.thickness * 2}px;
+        height: ${props.thickness * 2}px;
+        opacity: 1;
+    }
+    
+    to {
+        top: 0;
+        left: 0;
+        width: 100%;
+        height: 100%;
+        opacity: 0;
+    }
+  `};
   animation-duration: 1s;
   animation-timing-function: cubic-bezier(0, 0.2, 0.8, 1);
   animation-iteration-count: infinite;
